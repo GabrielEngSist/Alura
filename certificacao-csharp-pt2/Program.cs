ff36f31c23a517a9b703158b8d5692c7189b9f6d
unknown <gabriel.engsist@gmail.com>
2020-03-22 17:05:07 -0300

    Add projeto aula 2 git

---
+++ certificacao-csharp-pt2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace certificacao_csharp_pt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
